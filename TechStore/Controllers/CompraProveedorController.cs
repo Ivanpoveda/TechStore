a5cb220a923b3e256177f26d3fd8d8642f55ce04
Ivan <Ivan@DESKTOP-P7OAFJE>
2026-08-11 22:45:47 -0600

    Ajustes finales

--- TechStore/Controllers/CompraProveedorController.cs
+++ TechStore/Controllers/CompraProveedorController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 using TechStore.Models;
 
 namespace TechStore.Controllers
